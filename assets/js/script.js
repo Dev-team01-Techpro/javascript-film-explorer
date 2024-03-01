8353fcca4c960cd83fdcb9514353ceb8388c8169
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2024-03-01 13:43:31 +0100

    Core

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// index.html mainde olusturulan
+// film elementlerini olusturacak javascript fonksiyonu
+
+/*
+    api.jsdeki butun filmleri ceken fonksiyonu 
+    calistiracak fonksiyon
+*/
+
+/*
+    api.jsdeki tek filmi ceken fonksiyonu 
+    calistiracak fonksiyon
+*/
+/*
+    api.jsdeki search apisini 
+    calistiracak fonksiyon
+*/
