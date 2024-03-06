6b1ca94af5ba6ed66a24f1e0402c3366795d9aa8
Eda Sarıkaya <edassarkaya@outlook.com>
2024-03-06 00:45:51 +0000

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+import { search } from "../../data/api.js";
+const inpMovieName = document.querySelector(".inp");
+const filmList = document.querySelector(".film-list");
+
 // index.html mainde olusturulan kartlarin
 // elementlerini olusturacak javascript fonksiyonu
 // createelement kullanacak Mehmet
@@ -15,7 +19,56 @@
     api.jsdeki search apisini 
     calistiracak fonksiyon // Eda
 */
-/*
-    api.jsdeki people apisini 
-    calistiracak fonksiyon // Cahit
-*/
+
+
+const fullFill = (films) => {
+
+    let str = "";
+
+   films.forEach(element => {
+    str+= ` <div class="card m-3 p-3 bg-danger-subtle " style="width: 18rem;">
+    <img src=${element.show.image.medium} class="card-img-top h-100 w-100 position-relative rounded-3  " alt="...">
+      <p class="card-text text-center py-1">${element.show.name}</p>
+  </div>`    
+   }); 
+
+   return str;
+}
+
+
+
+
+//----------------------Eda-----------------------
+
+let vrb = "";
+
+inpMovieName.addEventListener("input",async (e)=>{
+
+   
+    clearTimeout(vrb)
+
+    vrb = setTimeout(async ()=>{
+       
+        let movies = e.target.value;
+
+        let moviesFiltered = await searchByName(movies);
+   
+     let all =  fullFill(moviesFiltered) 
+     filmList.innerHTML=all;
+    }
+    ,1000)
+
+});
+
+
+const searchByName = async (q)=>{
+   let searchedMovie = await search(q);
+   return searchedMovie;
+ } 
+
+ 
+
+//----------------------Eda------------------------
+
+
+
--- data/api.js
+++ data/api.js
@@ -1,10 +1,30 @@
-const API_BASE_URL = "https://api.tvmaze.com";
+const API_BASE_URL = "https://api.tvmaze.com/";
 
 // ----------------------------------------------
 // Butun Filmleri cek // Sule
 // ----------------------------------------------
 // Tek Film cek // Duygu
 // ----------------------------------------------
+
 // Search apisi // Eda
-// ----------------------------------------------
+//----------------------Eda-----------------------
+
+export const search = async (q) => {
+
+    if(q.length < 3) return;
+
+
+        const res = await fetch(`${API_BASE_URL}/search/shows?q=:${q}`);
+
+        if(!res) throw new Error("data is empty");
+
+        const data = await res.json();
+
+        return data;
+}
+
+
+
+//----------------------Eda------------------------
+
 // People verisini cek // Cahit
