3227cd4389be71d197d46bd3da9bba37dcb0d16d
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-03-08 17:00:27 +0300

    get movie by name

--- data/api.js
+++ data/api.js
@@ -3,8 +3,20 @@ const API_BASE_URL = "https://api.tvmaze.com";
 // ----------------------------------------------
 // Butun Filmleri cek // Sule
 // ----------------------------------------------
-// Tek Film cek // Duygu
+// ---------------------Duygu-------------------------
+// Tek Film cek isim ile // Duygu
+
+    const getMovieByName = async (q) => {
+      const res = await fetch(`${API_BASE_URL}/search/shows?q=${q}`);
+      if(!res.ok) throw new Error("The movie can not be found"); 
+      const data= await res.json();
+      return data;
+    }
+    
+// --------------------Duygu--------------------------
 // ----------------------------------------------
 // Search apisi // Eda
 // ----------------------------------------------
 // People verisini cek // Cahit
+
+export{getMovieByName};
